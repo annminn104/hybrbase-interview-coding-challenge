087cca2a31867a3b56c3ebf7485e33da476e2569
minhmatthewsb <minh.nca@sanbul.net>
2024-06-03 20:40:16 +0700

    fix: fix env

--- src/common/environments/index.ts
+++ src/common/environments/index.ts
@@ -26,30 +26,29 @@ const configSchema = z.object({
 
 const configEnv = configSchema.safeParse({
   base: {
-    client: process.env.NEXT_PUBLIC_BASE_CLIENT
+    client: process.env.NEXT_PUBLIC_BASE_CLIENT || 'public'
   },
   email: {
-    send: process.env.GMAIL_EMAIL_SEND,
-    password: process.env.GMAIL_EMAIL_PASSWORD
+    send: process.env.NEXT_PUBLIC_GMAIL_EMAIL_SEND || 'public',
+    password: process.env.NEXT_PUBLIC_GMAIL_EMAIL_PASSWORD || 'public'
   },
   googleSheet: {
-    sheetId: process.env.GG_SHEET_ID,
-    type: process.env.GG_TYPE,
-    projectId: process.env.GG_PROJECT_ID,
-    privateKeyId: process.env.GG_PRIVATE_KEY_ID,
-    privateKey: process.env.GG_PRIVATE_KEY,
-    clientEmail: process.env.GG_CLIENT_EMAIL,
-    clientId: process.env.GG_CLIENT_ID,
-    authUri: process.env.GG_AUTH_URI,
-    tokenUri: process.env.GG_TOKEN_URI,
-    authProviderX509CertUrl: process.env.GG_AUTH_PROVIDER_X509_CERT_URL,
-    clientX509CertUrl: process.env.GG_CLIENT_X509_CERT_URL,
-    universeDomain: process.env.GG_UNIVERSE_DOMAIN
+    sheetId: process.env.NEXT_PUBLIC_GG_SHEET_ID,
+    type: process.env.NEXT_PUBLIC_GG_TYPE,
+    projectId: process.env.NEXT_PUBLIC_GG_PROJECT_ID,
+    privateKeyId: process.env.NEXT_PUBLIC_GG_PRIVATE_KEY_ID,
+    privateKey: process.env.NEXT_PUBLIC_GG_PRIVATE_KEY,
+    clientEmail: process.env.NEXT_PUBLIC_GG_CLIENT_EMAIL,
+    clientId: process.env.NEXT_PUBLIC_GG_CLIENT_ID,
+    authUri: process.env.NEXT_PUBLIC_GG_AUTH_URI,
+    tokenUri: process.env.NEXT_PUBLIC_GG_TOKEN_URI,
+    authProviderX509CertUrl: process.env.NEXT_PUBLIC_GG_AUTH_PROVIDER_X509_CERT_URL,
+    clientX509CertUrl: process.env.NEXT_PUBLIC_GG_CLIENT_X509_CERT_URL,
+    universeDomain: process.env.NEXT_PUBLIC_GG_UNIVERSE_DOMAIN
   }
 });
 
 if (!configEnv.success) {
-  console.error(configEnv);
   throw new Error('ENV Not Found!');
 }
 
